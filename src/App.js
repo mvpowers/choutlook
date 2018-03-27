6a0e0025a6c6664dd270f4035880b6fd8e0e21ed
Mikey Powers <michaelvincentpowers@gmail.com>
2018-03-27 10:12:51 -0700

    start message bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import NewMessageBar from './components/NewMessageBar';
 
 const App = () => (
   <div className="App">
     <Navbar />
+    <NewMessageBar />
   </div>
 );
 
--- src/components/NewMessageBar.js
+++ src/components/NewMessageBar.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FaTh, FaCog, FaQuestion, FaSkype } from 'react-icons/lib/fa';
+import { FaBars } from 'react-icons/lib/fa';
 
 const Container = styled.div`
   display: flex;
-  justify-content: space-between;
-  background-color: #0078d7;
-  color: #fff;
-`;
-
-const Section = styled.div`
-  display: flex;
   flex-direction: row;
-  align-items: center;
+  background-color: #f4f4f4;
+  color: #5084aa;
 `;
 
 const Icon = styled.div`
@@ -23,38 +17,10 @@ const Icon = styled.div`
   }
 `;
 
-const Title = styled.div`
-  font-size: 17px;
-  margin: 0 30px;
-`;
-
-const StyledInput = styled.input`
-  background-color: #b3d7f3;
-  border: 1px solid #0078d7;
-  border-radius: 4px;
-  padding: 5px;
-  color: #333;
-`;
-
 export default () => (
   <Container>
-    <Section>
-      <Icon>
-        <FaTh size={16} />
-      </Icon>
-      <Title>Choutlook</Title>
-      <StyledInput type="text" placeholder="what up" />
-    </Section>
-    <Section>
-      <Icon>
-        <FaSkype size={16} />
-      </Icon>
-      <Icon>
-        <FaCog size={16} />
-      </Icon>
-      <Icon>
-        <FaQuestion size={16} />
-      </Icon>
-    </Section>
+    <Icon>
+      <FaBars size={16} />
+    </Icon>
   </Container>
 );
