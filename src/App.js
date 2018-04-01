91678f7fdd03de3186758069b4aa697a2ff596fd
Mikey Powers <michaelvincentpowers@gmail.com>
2018-04-01 13:49:14 -0700

    encrypting

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import MessageList from './components/MessageList';
 import MessageView from './components/MessageView';
 
 require('dotenv').config();
+const crypto = require('crypto');
 
 const serverHost = process.env.SERVER_HOST || '127.0.0.1';
 const serverPort = process.env.SERVER_PORT || '3001';
@@ -39,6 +40,7 @@ export default class App extends Component {
     this.state = {
       displayMsg: testData,
       sendMsg: '',
+      password: '',
     };
   }
 
@@ -59,7 +61,11 @@ export default class App extends Component {
   };
 
   handleSend = () => {
-    socket.emit('createMessage', this.state.sendMsg);
+    const cipher = crypto.createCipher('aes192', this.state.password);
+    let encrypted = cipher.update(this.state.sendMsg, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+
+    socket.emit('createMessage', encrypted);
     this.setState({ sendMsg: '' });
   };
 
@@ -69,7 +75,11 @@ export default class App extends Component {
 
   handleKeyPress = e => {
     if (!e.shiftKey && e.key === 'Enter') {
-      socket.emit('createMessage', this.state.sendMsg);
+      const cipher = crypto.createCipher('aes192', this.state.password);
+      let encrypted = cipher.update(this.state.sendMsg, 'utf8', 'hex');
+      encrypted += cipher.final('hex');
+
+      socket.emit('createMessage', encrypted);
       this.setState({ sendMsg: '' });
     }
   };
